a5458edf2b076e8750333eaf0ce8833ba2c0afd9
RTCRhino <39653967+RTCRhino@users.noreply.github.com>
2018-05-26 22:13:18 +0100

    Python Script to make a json file

---
+++ TVShowTracker/TvShowList.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import io
+
+for counter in range(150):
+    url = "http://api.tvmaze.com/shows?page=" + str(counter)
+    print(url)
+    r = requests.get(url)
+
+    with io.open("tvList.txt", "w", encoding="utf-8") as f:
+        f.write(r.text)
+    with io.open("tvList.txt", "r", encoding="utf-8") as f:
+        j = json.load(f)
+
+    with open("TvShowResults.txt", "r") as f:
+        TvShowList = json.load(f)
+
+    for item in j:
+        TvShowList[item["name"]] = item["id"]
+
+    with open("tvShowResults.txt", "w") as outfile:
+        json.dump(TvShowList, outfile)
