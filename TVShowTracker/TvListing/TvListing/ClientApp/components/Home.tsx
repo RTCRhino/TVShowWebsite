01c11baecacbf06ee32a93f5119fae9f6c164b48
RTCRhino <39653967+RTCRhino@users.noreply.github.com>
2018-05-28 15:01:02 +0100

    Changes to website including layout options

--- TVShowTracker/TvListing/TvListing/ClientApp/components/Home.tsx
+++ TVShowTracker/TvListing/TvListing/ClientApp/components/Home.tsx
@@ -4,7 +4,15 @@ import { RouteComponentProps } from 'react-router';
 export class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div className='content'>
-            <p>Hello</p>
+            <div className="row">
+                <div className = "col-sm-3">
+                    <button className="btn-block program-button"></button>
+                </div>
+                <div className = "col-sm-9 program-details-frame">
+                    <h3>Program name</h3>
+                    <p>Description of your next epsiode</p>
+                </div>
+            </div>
         </div>;
     }
 }
